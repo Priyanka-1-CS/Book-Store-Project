7159592d783cf64af0990e48330f3fc7a2cf1c2c
Priyanka M <priyankamukherjee024@gmail.com>
2025-05-27 16:27:57 +0530

    index  modified

--- index.php
+++ index.php
@@ -93,7 +93,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/stylel.css">
+    <link rel="stylesheet" href="css/styler.css">
     <title>Login</title>
 </head>
 <body>
